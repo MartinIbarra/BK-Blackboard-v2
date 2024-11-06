11f5de0ebfbc545e5b0fb29fda30767e499735cb
MartinIbarra <ibarramartin92@gmail.com>
2024-11-06 01:05:24 -0300

    ls

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const app = express();
 const server = require("http").createServer(app);
 const dotenv = require("dotenv");
 dotenv.config({ path: [".env.development", ".env.production"] });
-
 const { initSocket, Users, Rooms } = require('./sockets');
+
+const origin = process.env.FRONT_ENDPOINT || 'http://localhost:5173'
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONT_ENDPOINT || "http://localhost:5173",
+    origin
   },
 });
 
@@ -19,7 +20,7 @@ initSocket(io);
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const corsOptions = {
-  origin: process.env.FRONT_ENDPOINT || "http://localhost:5173",
+  origin,
   credentials: true,
   optionsSuccessStatus: 200,
 };
@@ -30,23 +31,6 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.set("port", process.env.PORT || 5000);
-
-// app.get("/protected", (req, res) => { });
-
-app.post("/login", (req, res) => {
-  if (req.body) {
-    const { name } = req.body;
-    Users.push({ name })
-    console.log(`Users => ${Users}`)
-    console.log('cred => ', req.body);
-  }
-  res.status(200).send("OK");
-});
-
-
-app.get("/test", (req, res) => {
-  res.status(200).send("OK");
-});
 
 server.listen(app.get("port"), () => console.log(`server listening on port ${app.get("port")}`));
 
--- sockets.js
+++ sockets.js
@@ -6,59 +6,63 @@ let Users = [];
 
 const initSocket = (io) => {
   io.on("connection", (socket) => {
-    console.log("user connected => ", socket.id);
+    //console.log("user connected => ", socket.id);
     // console.log("Users => ", Users);
 
     // Add user to the list
     Users.push({ id: socket.id });
-
-    socket.emit("room_list", Rooms);
+    //console.log(`Users => ${JSON.stringify(Users)}`)
+    // socket.emit("room_list", Rooms);
 
     // Return the rooms list every 2s
     setInterval(() => {
       socket.emit("room_list", Rooms);
-    }, 2000);
+    }, 1500);
 
-    socket.on("createRoom", ({ room, socket_name }) => {
-      // console.log("==== room created ====", room);
-      // console.log("==== user created ====", socket_name);
-
+    socket.on("createRoom", ({ room, socket_cred }) => {
       // Add name to the user
+      //console.log(socket_cred)
       const userIdx = Users.findIndex((e) => e.id === socket.id);
-      Users[userIdx].name = socket_name;
+      Users[userIdx].name = socket_cred.name;
       Users[userIdx].room = room;
 
       io.to(room).emit("joinRoom");
       const room_uuid = uuid()
       Rooms.push({ room, id: room_uuid });
+      console.log(JSON.stringify(Rooms))
 
       socket.emit("room_list", Rooms);
-
-      // console.log("Users => ", Users);
-      // console.log("Rooms => ", Rooms);
     });
 
-    socket.on("joinRoom", ({ room, socket_name }) => {
+    socket.on("joinRoom", ({ room, socket_cred }) => {
+      //console.log(`socket_cred in joinRoom => ${JSON.stringify(socket_cred)}`)
       const userIdx = Users.findIndex((e) => e.id === socket.id);
       if (userIdx === -1) {
-        Users.push({ id: socket.id, name: socket_name, room });
+        Users.push({ id: socket.id, name: socket_cred.name, room });
       } else {
-        Users[userIdx].name = socket_name;
+        Users[userIdx].name = socket_cred.name;
         Users[userIdx].room = room;
       }
-
       socket.join(room);
       io.to(room).emit("newSocketList", Users);
-
+      console.log(`Rooms ${JSON.stringify(Rooms)}`)
       socket.emit("room_list", Rooms);
-
-      // io.to(room).emit("joinRoom");
-      // console.log("Users join => ", Users);
-      // console.log("Rooms join => ", Rooms);
-      // console.log(`socket ${socket.id} has joined room ${room}`);
     });
 
+    socket.on("leaveRoom", ({ room, socket_cred }) => {
+      const userIdx = Users.findIndex((e) => e.id === socket.id)
+      for (let i = 0; i < Users.length; i++) {
+        if (userIdx !== -1) {
+          Users.splice(1, userIdx)
+        }
+      }
+      console.log(`exit room => ${room}`)
+      console.log(`Room => ${Rooms}`)
+      console.log(`socket_cred => ${socket_cred}`)
+    })
+
     socket.on("dibujandoSocket", (data) => {
+      //console.log(`dibujandoSocket ${JSON.stringify(data)}`)
       io.to(data.room).emit("dibujandoSocket", data);
     });
 
@@ -71,6 +75,7 @@ const initSocket = (io) => {
     });
 
     socket.on("disconnect", (data) => {
+      console.log(`data on disconnect => ${data}`)
       Users = Users.filter((e) => e.id !== socket.id); // Delete the user
 
       if (Users.length === 0) {
@@ -84,15 +89,8 @@ const initSocket = (io) => {
           }
         }
       }
-
       socket.emit("room_list", Rooms);
-
-      // console.log("Users onDelete => ", Users);
-      // console.log("Rooms onDelete => ", Rooms);
-
-      // console.log("disconnected => ", data);
     });
   });
-
 }
 module.exports = { initSocket, Users, Rooms };
